7da33ca1f5c61d52c6ffe83956ccb8768feb1b7d
Alpeshsorte <alpeshsorte4@gmail.com>
2026-04-17 17:35:09 +0530

    update code

---
+++ git_push_pracice2/script.js
@@ -0,0 +1,7 @@
+let message=document.getElementById("but")
+// message.innerText="message print succeesfully"
+let val;
+function fun() {
+    alert("hi follow car")
+    // let val="hi follow car"
+}
