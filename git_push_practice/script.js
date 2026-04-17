aafc8a5bd1297b94a4d1c1af083c6041d25e0721
Alpeshsorte <alpeshsorte4@gmail.com>
2026-04-17 16:34:48 +0530

    code

---
+++ git_push_practice/script.js
@@ -0,0 +1,5 @@
+let names=document.getElementById('name')
+    names.innerText="ALpesh sorte"
+let name2=document.getElementById("name")
+    name2.innerText="hello my dear friends"
+    // names.style.color=
