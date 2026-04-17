c659d09956aa9fb1be09df0fabe6ca2b5842cf31
Alpeshsorte <alpeshsorte4@gmail.com>
2026-04-17 17:42:14 +0530

    code

--- git_push_pracice2/script.js
+++ git_push_pracice2/script.js
@@ -1,5 +1,8 @@
 let message=document.getElementById("but")
 // message.innerText="message print succeesfully"
+
+let name_car=document.getElementById("mes")
+ name_car.innerText="car"
 let val;
 function fun() {
     alert("hi follow car")
