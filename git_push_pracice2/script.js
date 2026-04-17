0cdc27f4d46ae56da92a01a402a726dd901332d7
Alpeshsorte <alpeshsorte4@gmail.com>
2026-04-17 17:46:30 +0530

    change

--- git_push_pracice2/script.js
+++ git_push_pracice2/script.js
@@ -8,3 +8,10 @@ function fun() {
     alert("hi follow car")
     // let val="hi follow car"
 }
+
+let but=document.getElementById("but")
+but.addEventListener("click", "add")
+
+function add{
+    alert(hi)
+}
